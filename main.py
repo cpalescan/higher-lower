143f3d72d68761273a5ac70aec402d7eec4bb88d
paraloopable <33279693-paraloopable@users.noreply.replit.com>
2024-04-26 12:05:01 +0000

    higher lower

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import random
 ## pick a random dictionary from data (list) and generate variables with the values. 
 ## Store "follower_count"
 
-def picker():
+def picker(random_number):
   """Picks a random dictionary from the list. Prints name, description and country. 
   Stores follower count"""
-  random_number = random.randint(0, len(data) - 1)
   chosen = data[random_number]
   name = chosen["name"]
   followers = chosen["follower_count"]
   description = chosen["description"]
   country = chosen["country"]
   print(f"{name}, a {description} from: {country}")
-  return followers
+  set = [name, description, country, followers, random_number]
+  return(set)
 
 ## user input
 def user_choice():
@@ -47,40 +47,61 @@ def check(picked_by_user, other_one):
   elif picked_by_user < other_one:
     print("Oh no, wrong answer!")
     return True
-    
-    
+
+
 
 ## starting conditions
   
 game_over = False
 score = 0
 
+print(logo)
+
+## define A
+random_number = random.randint(0, len(data) - 1)
+set_a = picker(random_number)
+a = set_a[3]
+
+print(vs)
+
+## define B (!= a)
+random_number = random.randint(0, len(data) - 1)
+while random_number == set_a[4]:
+  random_number = random.randint(0, len(data) - 1)
+set_b = picker(random_number)
+b = set_b[3]
+
 
 ## body
-print(logo)
-print("")
-print("")
-print("")
-while game_over == False:
-
-    
-  a = picker()
-  print("")
-  print(vs)
-  print("")
-  b = picker()
-  if b == a:
-    b = picker()
-    
+  
+while game_over == False:    
+## user choice
   chosen_one = user_choice()
   if chosen_one == a:
     other = b
+    chosen_set = set_a
   elif chosen_one == b:
     other = a
-  
+    chosen_set = set_b
+
+## checking the answer
   game_over = check(chosen_one, other)
 
-
+## replacing A with the right answer and generating a new B
+  if game_over == False:
+  
+    a = chosen_one
+    set_a = chosen_set
+    print(f"{set_a[0]}, a {set_a[1]} from: {set_a[2]}")
+    
+    random_number = random.randint(0, len(data) - 1)
+    while random_number == set_a[4]:
+      random_number = random.randint(0, len(data) - 1)
+    set_b = picker(random_number)
+    b = set_b[3]
+   
+  
+## end
 
 print(f"Your final score is {score}")
 
